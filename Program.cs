9a6e80b6c0a293b9fe4c8ec52f874214bfd7fa9f
Sukanta Bala <sukantabalacste2016@gmail.com>
2016-09-15 00:35:19 +0600

    Program.cs file changed

--- Program.cs
+++ Program.cs
@@ -10,6 +10,18 @@ namespace Polymorphism_OOP_Example
     {
         static void Main(string[] args)
         {
+            int number1 = 10;
+            int number2 = 20;
+            int sum= summation(number1,number2);
+            Console.WriteLine(sum);
+            Console.ReadKey();
+
+        }
+
+        public static int summation(int nm1,int nm2)
+        {
+            int result = nm1 + nm2;
+            return result;
         }
     }
 }
